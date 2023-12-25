b4d8e0f537d7e9fb89d3c86f1aad15ed9cd14c8c
Saijeet Upadhyay <saijeet2019@gmail.com>
2023-12-25 18:07:54 +0545

    Complete : grid layout

--- wp-content/themes/default/front-page.php
+++ wp-content/themes/default/front-page.php
@@ -3,6 +3,30 @@ get_header();
 the_post();
 ?>
 <main class="page__home">
-    Hello World
+    <section class="container">
+        <div class="grid-layout">
+            <div class="grid-item">
+                <img class="alignright" src="https://via.placeholder.com/200" alt="" /></p>
+            </div>
+            <div class="grid-item">
+                <h2 class="grid-item__title">THE STYLE EDIT</h2>
+                <p>Shop the looks right off our embody girls</p>
+                <a href="#">Shop now</a>
+            </div>
+            <div class="grid-item">
+                <img class="alignright" src="https://via.placeholder.com/200" alt="" /></p>
+            </div>
+            <div class="grid-item">
+                <img class="alignright" src="https://via.placeholder.com/200" alt="" /></p>
+            </div>
+            <div class="grid-item">
+                <img class="alignright" src="https://via.placeholder.com/200" alt="" /></p>
+            </div>
+            <div class="grid-item">
+                <img class="alignright" src="https://via.placeholder.com/200" alt="" /></p>
+            </div>
+
+        </div>
+    </section>
 </main>
 <?php get_footer(); ?>
