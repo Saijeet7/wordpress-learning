6761f741370005942ca0adf60b5ea75e63187cbd
Saijeet Upadhyay <saijeet2019@gmail.com>
2024-01-02 21:11:16 +0545

    Complete : Events post types

--- wp-content/themes/fictional-university-theme/page-past-events.php
+++ wp-content/themes/fictional-university-theme/page-past-events.php
@@ -7,11 +7,11 @@ get_header(); ?>
         style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
     <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title">
-            All Events
+            Past Events
         </h1>
         <div class="page-banner__intro">
             <p>
-                See what is going on in our world
+                A Recap of Past Events
             </p>
         </div>
     </div>
@@ -19,8 +19,27 @@ get_header(); ?>
 
 <div class="container container--narrow page-section">
     <?php
-    while (have_posts()) {
-        the_post(); ?>
+    $today = date('Ymd');
+    $pastEvents = new WP_Query(
+        array(
+            'paged' => get_query_var('paged', 1),
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC',
+            'meta_query' => array(
+                array(
+                    'key' => 'event_date',
+                    'compare' => '<',
+                    'value' => $today,
+                    'type' => 'numeric',
+                )
+            )
+        )
+    );
+
+    while ($pastEvents->have_posts()) {
+        $pastEvents->the_post(); ?>
         <div class="event-summary">
             <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
                 <span class="event-summary__month">
@@ -46,11 +65,12 @@ get_header(); ?>
             </div>
         </div>
     <?php }
-    echo paginate_links();
+    echo paginate_links(
+        array(
+            'total' => $pastEvents->max_num_pages
+        )
+    );
     ?>
-    <hr class="section-break">
-    <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events') ?>">Check out past events
-            archieve</a>.</p>
 
 
 </div>
