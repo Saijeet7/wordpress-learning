31cbf8b3a86f834d4d1fb9557c8f4ce41f993b68
Saijeet Upadhyay <saijeet2019@gmail.com>
2024-01-04 20:05:17 +0545

    Complete : professor post type

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -39,6 +39,22 @@ function university_post_types()
             'menu_icon' => 'dashicons-awards'
         )
     );
+    register_post_type(
+        'professor',
+        array(
+            'show_in_rest' => true,
+            'supports' => array('title', 'editor'),
+            'public' => true,
+            'labels' => array(
+                'name' => 'Professors',
+                'add_new_item' => 'Add New Professors',
+                'edit_item' => 'Edit Professor',
+                'all_items' => 'All Professors',
+                'singular_name' => 'Professor'
+            ),
+            'menu_icon' => 'dashicons-welcome-learn-more'
+        )
+    );
 }
 
 
