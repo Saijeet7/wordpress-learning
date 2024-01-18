19bbd37971dba1c4bb1cfb81ff9aeff88e8cab0c
Saijeet Upadhyay <saijeet2019@gmail.com>
2024-01-18 22:16:53 +0545

    Complete :search awware relationship

--- wp-content/themes/fictional-university-theme/inc/search-route.php
+++ wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -22,6 +22,8 @@ function universitySearchResults($data)
             's' => sanitize_text_field($data['term'])
         )
     );
+
+
     $results = array(
         'generalInfo' => array(),
         'professors' => array(),
@@ -96,5 +98,35 @@ function universitySearchResults($data)
 
         }
     }
+
+    $programRelationshipQuery = new WP_Query(
+        array(
+            'post_type' => 'professor',
+            'meta_query' => array(
+                array(
+                    'key' => 'related_programs',
+                    'compare' => 'LIKE',
+                    'value' => '87',
+                )
+            )
+        )
+    );
+    while ($programRelationshipQuery->have_posts()) {
+        $programRelationshipQuery->the_post();
+        if (get_post_type() == 'professor') {
+            array_push(
+                $results['professors'],
+                array(
+                    'title' => get_the_title(),
+                    'permalink' => get_the_permalink(),
+                    'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+                )
+            );
+        }
+    }
+
+    $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+
+
     return $results;
 }
